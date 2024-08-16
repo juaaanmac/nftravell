f8f58c7387ca74dcbe19be2c7014d92e4ac6c9c1
juaaanmac <juanmacri@outlook.com>
2024-08-16 15:11:05 -0300

    new script for deployment

--- script/Deploy.NFT.s.sol
+++ script/Deploy.NFT.s.sol
@@ -5,14 +5,14 @@ import {Script, console} from "forge-std/Script.sol";
 import {PriceFeed} from "../src/PriceFeed.sol";
 import {NFTravell} from "../src/NFTravell.sol";
 
-contract DeployScript is Script {
+contract DeployNFTScript is Script {
     function setUp() public {}
 
     function run() public {
         uint256 deployerPrivateKey = vm.envUint("ADMIN_PRIVATE_KEY");
         vm.startBroadcast(deployerPrivateKey);
 
-        address priceFeedAddress = vm.envAddress("PRICE_FEED");
+        address priceFeedAddress = vm.envAddress("SEPOLIA_PRICE_FEED");
         PriceFeed priceFeed = new PriceFeed(priceFeedAddress);
 
         new NFTravell(address(priceFeed), 2, "Villa la Angostura Token", "VLAT");
---
+++ script/Deploy.Rewards.s.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Script, console} from "forge-std/Script.sol";
+import {RewardsToken} from "../src/RewardsToken.sol";
+
+contract DeployRewardsScript is Script {
+    function setUp() public {}
+
+    function run() public {
+        uint256 deployerPrivateKey = vm.envUint("ADMIN_PRIVATE_KEY");
+        address callbackSenderAddress = vm.envAddress("REACTIVE_CALLBACK_SENDER");
+
+        vm.startBroadcast(deployerPrivateKey);
+
+        new RewardsToken("Neuquen Rewards Token", "NQNR", callbackSenderAddress);
+
+        vm.stopBroadcast();
+    }
+}
