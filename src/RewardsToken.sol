b14f6926d382ac1a22a0e396eb350b46974ad972
juaaanmac <juanmacri@outlook.com>
2024-08-19 12:28:58 -0300

    giving rewards according to day of year

--- src/RewardsToken.sol
+++ src/RewardsToken.sol
@@ -5,11 +5,10 @@ import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
 contract RewardsToken is ERC20, Ownable {
-    uint256 internal _rewardAmount = 100;
     address private _callbackSender;
 
     modifier onlyReactive() {
-        if (_msgSender() != _callbackSender ) {
+        if (_msgSender() != _callbackSender) {
             revert InvalidCallabackSender(_msgSender());
         }
         _;
@@ -18,7 +17,7 @@ contract RewardsToken is ERC20, Ownable {
     error InvalidCallabackSender(address sender);
 
     constructor(string memory name_, string memory symbol_, address callbackSender_)
-        ERC20(name_,symbol_)
+        ERC20(name_, symbol_)
         Ownable(_msgSender())
     {
         _callbackSender = callbackSender_;
@@ -27,8 +26,25 @@ contract RewardsToken is ERC20, Ownable {
     function setCallbackSender(address callbackSender) public onlyOwner {
         _callbackSender = callbackSender;
     }
-    
-    function reward(address account) external onlyReactive() {
-        _mint(account, _rewardAmount);
+
+    function reward(address account) external onlyReactive {
+        _mint(account, _rewardAmount());
+    }
+
+    function _rewardAmount() internal view returns (uint256) {
+        uint256 secondsPerDay = 60 * 60 * 24;
+        uint256 secondsPerYear = 31557600; // 60 * 60 * 24 * 365.25
+        uint256 daysPerYeear = 365;
+
+        /**
+         * by doing the division before the multiplication, I get the timestamp of the first day 
+         * of the current year and then I get the number of seconds elapsed until that day
+         */
+        uint256 dias = (block.timestamp / secondsPerYear) * secondsPerYear;
+
+        // get the number of days elapsed since the first day of the current year
+        uint256 daysElapsed = (block.timestamp - dias) / secondsPerDay + 1;
+
+        return daysPerYeear - daysElapsed;
     }
 }
